40eaae573209072aabd895ee791de0fc01e6ba5d
Nazhif Haidar Putra Wibowo <nzhifhaidar@gmail.com>
2024-09-04 00:26:54 +0700

    feat: Integrasi sebagian dengan backend

--- src/services/course-handlers.ts
+++ src/services/course-handlers.ts
@@ -13,3 +13,22 @@ export const getCourses = async () => {
         throw new Error(`Error while getting courses. ${error}`)
     }
 }
+
+export const getDashboard = async (username: string, token: string) => {
+    try {
+        const response = await axios.get(`${API_URI}/dashboard/${username}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            timeout: 10000
+        });
+
+        return {
+            status: response.status,
+            data: response.data
+        }
+    } catch(error: any) {
+        throw new Error(`Error while getting dashboard. ${error}`)
+    }
+}
